382b4810e3ba7e0a7b1df2f8f48f65939a1f37fa
tamsynrattu05 <tamsynrattu05@gmx.com>
2025-03-06 08:55:54 +0000

    Add 2.java

---
+++ src/2.java
@@ -0,0 +1,14 @@
+package com;
+import java.util.Scanner;
+public class RandomCode {
+    public static void main(String[] args) {
+        Scanner sc = new Scanner(System.in);
+        System.out.print("Enter a number: ");
+        int num = sc.nextInt();
+        if (num % 2 == 0) {
+            System.out.println("Even");
+        } else {
+            System.out.println("Odd");
+        }
+    }
+}
